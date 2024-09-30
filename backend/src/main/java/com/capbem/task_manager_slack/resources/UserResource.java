b5ae9ced2b58e569528714ffa507eac08c92855a
ericomurta1991 <erico_murta@yahoo.com.br>
2024-09-30 15:20:05 -0300

    User Controller

---
+++ backend/src/main/java/com/capbem/task_manager_slack/resources/UserResource.java
@@ -0,0 +1,69 @@
+package com.capbem.task_manager_slack.resources;
+
+import java.net.URI;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.capbem.task_manager_slack.dto.UserDTO;
+import com.capbem.task_manager_slack.services.UserService;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	private UserService service;
+
+	@GetMapping
+	public ResponseEntity<Page<UserDTO>> findAll(@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
+			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy) {
+
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Page<UserDTO> list = service.findAllPaged(pageRequest);
+		return ResponseEntity.ok().body(list);
+
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable Long id) {
+
+		UserDTO userDto = service.findById(id);
+		return ResponseEntity.ok().body(userDto);
+
+	}
+
+	@PostMapping
+	public ResponseEntity<UserDTO> insert(@RequestBody UserDTO userDto) {
+		userDto = service.insert(userDto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(userDto.getId())
+				.toUri();
+		return ResponseEntity.created(uri).body(userDto);
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> update(@PathVariable Long id, @RequestBody UserDTO userDto) {
+		userDto = service.update(id, userDto);
+		return ResponseEntity.ok().body(userDto);
+	}
+
+	@DeleteMapping(value = "{/id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+
+	}
+
+}
